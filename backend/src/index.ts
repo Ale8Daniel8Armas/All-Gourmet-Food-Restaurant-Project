4ade7d3b50a673fc4d5d62f6a6c349e5a48c9694
Daniel_Armas <105186962+Ale8Daniel8Armas@users.noreply.github.com>
2024-07-29 21:59:45 -0500

    Segundo avance con user profile en desarrollo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Conexión a la base de datos exitosa!"));
@@ -10,9 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello"});
-});
+app.use("api/my/user", myUserRoute);
 
 app.listen(2002, () => {
     console.log("servidor encendido en el puerto localhost:2002");
